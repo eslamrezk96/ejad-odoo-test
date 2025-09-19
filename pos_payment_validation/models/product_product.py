fb51f4498a5943be07161bf952ca35062a3f44d9
Eslam <eslam.rezk96@hotmail.com>
2025-09-19 20:19:00 +0300

    Send notification to the warehouses admin to check the ROP of the product less than 5

--- pos_payment_validation/models/product_product.py
+++ pos_payment_validation/models/product_product.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, api
+from markupsafe import Markup
 
 
 class ProductProduct(models.Model):
@@ -11,7 +12,21 @@ class ProductProduct(models.Model):
         product_available = self.env['stock.quant'].search([
             ('product_id', 'in', products), ('location_id.usage', '=', 'internal'), ('quantity', '<=', qty_limit_count)
         ])
-        product_label = []
-        for product in product_available:
-            product_label.append(product.product_id.name)
-        return ' -\n'.join(product_label)
+        if product_available:
+            products = product_available.mapped('product_id').ids
+            self._send_message_rop(products)
+        return ' -\n'.join(p.product_id.name for p in product_available)
+
+    def _send_message_rop(self, products_ids):
+        products = self.env['product.product'].browse(products_ids)
+        links = ", ".join(p._get_html_link() for p in products)
+        msg = Markup(f"These products are under the Re-Order Point: {links}")
+        partners = self.env.ref('stock.group_stock_manager').mapped('users.partner_id')
+        for partner in partners:
+            chan_info = self.env['discuss.channel'].channel_get(partners_to=[partner.id])
+            channel = self.env['discuss.channel'].browse(chan_info['id'])
+            channel.message_post(
+                body=msg,
+                message_type="comment",
+                subtype_xmlid="mail.mt_comment",
+            )
