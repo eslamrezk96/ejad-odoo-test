6d1f6c0738fb494c005b6c171d31ef4c34e2709a
Eslam <eslam.rezk96@hotmail.com>
2025-09-20 13:39:33 +0300

    Inventory => Task#1 In stock.picking add validation on picking type = “outgoing” && picking date >= today

---
+++ stock_picking_validation/__manifest__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Stock Picking Validation",
+    'description': """In stock.picking add validation on picking type =
+                    “outgoing” && picking date >= today""",
+    'depends': ['stock'],
+
+}
---
+++ stock_picking_validation/models/stock_picking.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models, api
+from odoo.exceptions import ValidationError
+
+
+class Picking(models.Model):
+    _inherit = 'stock.picking'
+
+    def button_validate(self):
+        for picking in self:
+            if picking.picking_type_code == 'outgoing':
+                if picking.scheduled_date.date() < fields.Date.today():
+                    raise ValidationError("You cannot validate an outgoing picking with a scheduled date earlier than today.")
+        return super(Picking, self).button_validate()
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        pickings = super(Picking, self).create(vals_list)
+        for picking in pickings:
+            if picking.picking_type_code == 'outgoing':
+                if picking.scheduled_date.date() < fields.Date.today():
+                    raise ValidationError("You cannot create an outgoing picking with a scheduled date earlier than today.")
+        return pickings
