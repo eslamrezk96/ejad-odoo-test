2923c572cbceccfdc5ec474a6e896240c346bd49
Eslam <eslam.rezk96@hotmail.com>
2026-07-05 09:20:51 +0300

    [update]

--- rop_products/models/product_template.py
+++ rop_products/models/product_template.py
@@ -9,6 +9,15 @@ class ProductTemplate(models.Model):
     has_rop = fields.Boolean("Has ROP")
     rop_count = fields.Integer("ROP Count")
 
+    product_rop_id = fields.Many2one('product.template')
+
+    product_rop_ids = fields.One2many(
+        comodel_name='product.template',
+        inverse_name='product_rop_id',
+        string='Product_rop_ids',
+
+        required=False)
+
     def action_get_rop_products(self):
         return {
             'type': 'ir.actions.act_window',
--- stock_picking_validation/models/stock_picking.py
+++ stock_picking_validation/models/stock_picking.py
@@ -22,3 +22,10 @@ class Picking(models.Model):
                 if picking.scheduled_date.date() < fields.Date.today():
                     raise ValidationError("You cannot create an outgoing picking with a scheduled date earlier than today.")
         return pickings
+
+    @api.constrains('scheduled_date', 'picking_type_code')
+    def _check_schedule_date(self):
+        for picking in self:
+            if picking.picking_type_code == 'outgoing':
+                if picking.scheduled_date.date() < fields.Date.today():
+                    raise ValidationError("You cannot set a scheduled date earlier than today for an outgoing picking.")
